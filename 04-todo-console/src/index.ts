d479d8d38c82e2b180cf833cc86a1b296db6ca5c
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-30 12:16:47 -0400

    feat: initial todo console app

---
+++ 04-todo-console/src/index.ts
@@ -0,0 +1,22 @@
+import { pauseMenu } from "./inquirerMenu";
+import Task from "./model/task";
+import Tasks from "./model/tasks";
+
+const main = async () => {
+
+  let opt
+  do {
+    // const { option } = await inquirerMenu();
+    // opt = option
+    const tasks = new Tasks();
+    const task = new Task("Comprar comida")
+
+    tasks._taskList[task.id] = task
+
+    console.log(tasks);
+
+    await pauseMenu()
+  } while (opt !== '0');
+}
+
+main();
