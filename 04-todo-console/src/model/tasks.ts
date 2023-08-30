896994cd08884f9892d713c20b1bcb65ca49880a
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-30 13:21:51 -0400

    feat: add create task

--- 04-todo-console/src/model/tasks.ts
+++ 04-todo-console/src/model/tasks.ts
@@ -1,13 +1,16 @@
-interface Task {
-  id: string;
-  description: string;
-  completedAt: Date | null;
-}
+import Task from './task';
+
 type TasksList = Record<string, Task>;
+
 class Tasks {
   _taskList: TasksList = {};
   constructor() {
     this._taskList = {};
+  }
+
+  createTask(description: string = '') {
+    const task = new Task(description);
+    this._taskList[task.id] = task;
   }
 }
 
