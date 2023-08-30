9923c906414b437ef65c7d3aa93ec72a9860c7b9
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-30 13:22:32 -0400

    feat: add timestamps

--- 04-todo-console/src/model/task.ts
+++ 04-todo-console/src/model/task.ts
@@ -1,13 +1,16 @@
 import { v4 as uuidv4 } from 'uuid';
-
 class Task {
-  id = '';
-  description = '';
-  completedAt = null;
+  id: string = '';
+  description: string = '';
+  createdAt: Date;
+  updatedAt: Date;
+  completedAt: Date | null = null;
 
   constructor(description: string) {
     this.id = uuidv4();
     this.description = description;
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
     this.completedAt = null;
   }
 }
