1614d6ee75844ceff336348934f208289a17dee3
Ariel Chura <ariel.chura.c@gmail.com>
2023-08-30 13:22:55 -0400

    feat: set 1,2,4 options

--- 04-todo-console/src/index.ts
+++ 04-todo-console/src/index.ts
@@ -1,19 +1,41 @@
-import { pauseMenu } from './inquirerMenu';
+import { inquirerMenu, pauseMenu, readInput } from './helpers/inquirerMenu';
 import Task from './model/task';
 import Tasks from './model/tasks';
 
 const main = async () => {
-  let opt;
+  let opt: string;
+  const tasks = new Tasks();
+
   do {
-    // const { option } = await inquirerMenu();
-    // opt = option
-    const tasks = new Tasks();
-    const task = new Task('Comprar comida');
+    // const task = new Task('Comprar comida');
+    // tasks._taskList[task.id] = task;
 
-    tasks._taskList[task.id] = task;
+    opt = await inquirerMenu();
 
-    console.log(tasks);
-
+    switch (opt) {
+      case '1':
+        const description = await readInput('Descripción:');
+        tasks.createTask(description);
+        break;
+      case '2':
+        console.log(tasks._taskList);
+      case '3':
+        break;
+      case '4':
+        const uncompletedTasks = Object.keys(tasks._taskList).reduce(
+          (acc, key) => {
+            const task = tasks._taskList[key];
+            if (task.completedAt === null) {
+              acc[key] = task;
+            }
+            return acc;
+          },
+          {} as Record<string, Task>
+        );
+        console.log(uncompletedTasks);
+      default:
+        break;
+    }
     await pauseMenu();
   } while (opt !== '0');
 };
