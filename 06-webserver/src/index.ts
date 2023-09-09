15adad73ea57a95fc991a38e8c67ee4efa7b9867
Ariel Chura <ariel.chura.c@gmail.com>
2023-09-09 17:36:49 -0400

    feat: implement handlebars - hbs

--- 06-webserver/src/index.ts
+++ 06-webserver/src/index.ts
@@ -1,20 +1,50 @@
 import express from 'express';
+import hbs from 'hbs';
 import path from 'path';
 
-const app = express();
 const port = 8080;
+const app = express();
 
-app.use('/', express.static('public/web-page'));
+// handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, '../views/partials'));
+
+// servir contenido estatico
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('/', (req, res) => {
+  res.render('home', {
+    title: 'Ariel Probando handlebars',
+    express: 'Express',
+    handlebars: 'Handlebars',
+  });
+});
 
 app.get('/generic', (req, res) => {
-  const genericPage = path.join(__dirname, '../public/web-page/generic.html');
-  res.sendFile(genericPage);
+  res.render('generic', {
+    title: 'generic',
+    express: 'Express',
+    handlebars: 'Handlebars',
+  });
 });
 
 app.get('/elements', (req, res) => {
-  const elementsPage = path.join(__dirname, '../public/web-page/elements.html');
-  res.sendFile(elementsPage);
+  res.render('elements', {
+    title: 'elements',
+    express: 'Express',
+    handlebars: 'Handlebars',
+  });
 });
+
+// app.get('/generic', (req, res) => {
+//   const genericPage = path.join(__dirname, '../public/web-page/generic.html');
+//   res.sendFile(genericPage);
+// });
+
+// app.get('/elements', (req, res) => {
+//   const elementsPage = path.join(__dirname, '../public/web-page/elements.html');
+//   res.sendFile(elementsPage);
+// });
 
 app.listen(port, () => {
   console.log(`Server is running in http://localhost:${port}`);
